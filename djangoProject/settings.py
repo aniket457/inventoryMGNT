af27386eba9f95bf23fa8b4c4fdadcd5fc24514e
Aniket Kadam <kaniket457@gmail.com>
2023-03-15 12:37:36 +0530

    added mysql database account info

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -74,13 +74,21 @@ WSGI_APPLICATION = "djangoProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "invoicemgnt",
+        "USER": "invoiceMGNT",
+        "PASSWORD": "invoiceMGNT457",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
